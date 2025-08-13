c591cf786ffa7815a194b030c60b02184ce8eb45
ElroyKR <elroykr@gmail.com>
2025-08-13 18:24:53 +0900

    fix: add @RequestBody annotation for create product api

--- src/main/java/kr/elroy/aigoya/product/ProductApi.java
+++ src/main/java/kr/elroy/aigoya/product/ProductApi.java
@@ -9,6 +9,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +31,7 @@ public interface ProductApi {
     @PostMapping("/create")
     ProductResponse createProduct(
             @Valid
-            @Parameter(description = "상품 정보", required = true)
+            @RequestBody
             CreateProductRequest request
     );
 }
