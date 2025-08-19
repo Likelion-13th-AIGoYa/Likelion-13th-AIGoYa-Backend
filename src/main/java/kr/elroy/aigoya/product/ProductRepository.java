805d48b62f772c93a52f0dd9b77c753da753e3dc
ElroyKR <elroykr@gmail.com>
2025-08-19 23:35:31 +0900

    fix: import 에러 정리

--- src/main/java/kr/elroy/aigoya/product/ProductRepository.java
+++ src/main/java/kr/elroy/aigoya/product/ProductRepository.java
@@ -1,5 +1,6 @@
 package kr.elroy.aigoya.product;
 
+import kr.elroy.aigoya.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
