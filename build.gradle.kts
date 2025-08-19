3ac562e8a2b519d633cc2a63634697beaf627cac
ElroyKR <elroykr@gmail.com>
2025-08-19 19:19:56 +0900

    feat: Add Jib plugin configuration for containerization

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.4"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.google.cloud.tools.jib") version "3.4.5"
 }
 
 group = "kr.elroy"
@@ -56,4 +57,52 @@ dependencyManagement {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jib {
+	from {
+		image = "amazoncorretto:21-alpine3.21"
+
+		platforms {
+			platform {
+				architecture = "arm64"
+				os = "linux"
+			}
+
+			platform {
+				architecture = "amd64"
+				os = "linux"
+			}
+		}
+	}
+
+	to {
+		image = "ghcr.io/likelion-13th-aigoya/backend"
+		tags = setOf("latest")
+
+		auth {
+			username = System.getenv("GHCR_USERNAME")
+			password = System.getenv("GHCR_TOKEN")
+		}
+	}
+
+	container {
+		jvmFlags =
+			listOf(
+				"-Dspring.profiles.active=prod",
+				"-Dserver.port=8080",
+				"-Djava.security.egd=file:/dev/./urandom",
+				"-Dfile.encoding=UTF-8",
+				"-Duser.timezone=Asia/Seoul",
+				"-XX:+UnlockExperimentalVMOptions",
+				"-XX:+UseContainerSupport",
+				"-XX:+UseG1GC",
+				"-XX:InitialHeapSize=1g",
+				"-XX:MaxHeapSize=1g",
+				"-XX:+DisableExplicitGC",
+				"-server",
+			)
+
+		ports = listOf("8080")
+	}
 }
