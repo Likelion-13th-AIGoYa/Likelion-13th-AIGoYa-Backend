a6214a2f92b6822a7e5775ded9c44381e6119db8
ElroyKR <elroykr@gmail.com>
2025-08-20 15:09:13 +0900

    fix: Store의 name의 중복을 허용하도록 수정

--- src/main/java/kr/elroy/aigoya/store/domain/Store.java
+++ src/main/java/kr/elroy/aigoya/store/domain/Store.java
@@ -5,9 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
 import jakarta.persistence.OneToMany;
 import kr.elroy.aigoya.order.domain.Order;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +28,7 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "name", nullable = false, unique = true)
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Column(name = "password", nullable = false)
