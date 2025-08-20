efc11a31788e116e43c39137ea8a005f8eb4d688
ElroyKR <elroykr@gmail.com>
2025-08-20 15:09:28 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/kr/elroy/aigoya/store/repository/StoreRepository.java
+++ src/main/java/kr/elroy/aigoya/store/repository/StoreRepository.java
@@ -2,12 +2,11 @@ package kr.elroy.aigoya.store.repository;
 
 import kr.elroy.aigoya.store.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-
     boolean existsByEmail(String email);
-    boolean existsByName(String name);
 
     Optional<Store> findByEmail(String email);
 }
