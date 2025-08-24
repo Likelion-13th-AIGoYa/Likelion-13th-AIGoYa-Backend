7061ec5810dfc5527fca42dc34843846735ae8d4
Stravinsken <Jsc78tt33@gmail.com>
2025-08-24 18:29:32 +0900

    refactor: 주석제거

--- src/main/java/kr/elroy/aigoya/analytics/dto/internal/WeatherInfo.java
+++ src/main/java/kr/elroy/aigoya/analytics/dto/internal/WeatherInfo.java
@@ -2,12 +2,9 @@ package kr.elroy.aigoya.analytics.dto.internal;
 
 import lombok.Builder;
 
-/**
- * WeatherService가 반환하는, 서비스 내부 통신용 날씨 정보 DTO
- */
 @Builder
 public record WeatherInfo(
-        Double temperature, // 기온
-        String summary      // 날씨 요약 ("맑음", "비" 등)
+        Double temperature,
+        String summary
 ) {
 }
